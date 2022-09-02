85dfd43cf1cb27e7323a661ab84fc0fac9dbbd39
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2022-09-02 22:33:25 +0300

    Добавлена работа с уже готовыми функциями СУБД для руководителя

--- CRM/leader/leaderWidget.cpp
+++ CRM/leader/leaderWidget.cpp
@@ -47,26 +47,28 @@ void leaderWidget::on_Provider_clicked() {
 
     ui->ProviderLabel->setText("Поставщики");
 
-    // TODO: fill table with db
-    // tableVendorUpdate();
-
-    // get from db
-    ui->tableWidget->setRowCount(5);
-    ui->tableWidget->setColumnCount(2);
-    ui->tableWidget->setColumnWidth(0, 91);
-    ui->tableWidget->setColumnWidth(1, 375);
-    QStringList Labels = {"id", "Name"};
+    ui->tableWidget->clear();
+    QStringList Labels = {"ID", "Название"};
+    ui->tableWidget->setColumnCount(Labels.size());
     ui->tableWidget->setHorizontalHeaderLabels(Labels);
 
-    // temporary
-    QTableWidgetItem* item = new QTableWidgetItem("123");
-    for (size_t i = 0; i < 5; i++) {
-        for (size_t j = 0; j < 2; j++) {
-            QTableWidgetItem* item = new QTableWidgetItem("123");
-            ui->tableWidget->setItem(i, j, item);
-            ui->tableWidget->item(i, j)->setFlags(Qt::ItemIsEnabled |
-                                                  Qt::ItemIsSelectable);
+    try {
+        soci::session session(*parent->database.get_pool().lock());
+        std::vector<provider> providers = getProvider(session, 0, 10);
+        ui->tableWidget->setRowCount(providers.size());
+
+        size_t current_row = 0;
+        for (const auto& current : providers) {
+            ui->tableWidget->setItem(
+                current_row, 0,
+                new QTableWidgetItem(QString::number(current.id)));
+            ui->tableWidget->setItem(
+                current_row, 1, new QTableWidgetItem(current.name.c_str()));
+            current_row++;
         }
+    } catch (const std::exception& e) {
+        QMessageBox::critical(this, "Ошибка", e.what());
+        return;
     }
 }
 
@@ -86,35 +88,55 @@ void leaderWidget::on_Deal_clicked() {
 
     ui->ProviderLabel->setText("Сделки");
 
-    // TODO: fill table with db
-    // tableDealUpdate();
-
-    // get from db
-    ui->tableWidget->setRowCount(5);
-    ui->tableWidget->setColumnCount(9);
-    QStringList Labels = {"id",     "id laptop", "price",  "customer", "status",
-                          "source", "date",      "seller", "mark?"};
+    ui->tableWidget->clear();
+    QStringList Labels = {"ID",         "Ноутбук", "Цена",
+                          "Покупатель", "Статус",  "Дата создания",
+                          "Продавец",   "Оценка",  "Последнее обновление"};
+    ui->tableWidget->setColumnCount(Labels.size());
     ui->tableWidget->setHorizontalHeaderLabels(Labels);
 
-    ui->tableWidget->setColumnWidth(0, 52);
-    ui->tableWidget->setColumnWidth(1, 52);
-    ui->tableWidget->setColumnWidth(2, 52);
-    ui->tableWidget->setColumnWidth(3, 52);
-    ui->tableWidget->setColumnWidth(4, 51);
-    ui->tableWidget->setColumnWidth(5, 52);
-    ui->tableWidget->setColumnWidth(6, 52);
-    ui->tableWidget->setColumnWidth(7, 52);
-    ui->tableWidget->setColumnWidth(8, 51);
+    try {
+        soci::session session(*parent->database.get_pool().lock());
+        std::vector<boost::tuple<deal, laptop, client, employee>> deals =
+            getDeal(session, 0, 10);
+        ui->tableWidget->setRowCount(deals.size());
 
-    // temporary
-    QTableWidgetItem* item = new QTableWidgetItem("123");
-    for (size_t i = 0; i < 5; i++) {
-        for (size_t j = 0; j < 9; j++) {
-            QTableWidgetItem* item = new QTableWidgetItem("123");
-            ui->tableWidget->setItem(i, j, item);
-            ui->tableWidget->item(i, j)->setFlags(Qt::ItemIsEnabled |
-                                                  Qt::ItemIsSelectable);
+        size_t current_row = 0;
+        for (const auto& [deal, laptop, customer, seller] : deals) {
+            ui->tableWidget->setItem(
+                current_row, 0, new QTableWidgetItem(QString::number(deal.id)));
+            ui->tableWidget->setItem(current_row, 1,
+                                     new QTableWidgetItem(laptop.name.c_str()));
+            ui->tableWidget->setItem(
+                current_row, 2,
+                new QTableWidgetItem(QString::number(deal.cost)));
+            ui->tableWidget->setItem(
+                current_row, 3, new QTableWidgetItem(customer.name.c_str()));
+            ui->tableWidget->setItem(current_row, 4,
+                                     new QTableWidgetItem(deal.status.c_str()));
+            ui->tableWidget->setItem(
+                current_row, 5,
+                new QTableWidgetItem((std::to_string(deal.created.day()) + "." +
+                                      std::to_string(deal.created.month()) +
+                                      "." + std::to_string(deal.created.year()))
+                                         .c_str()));
+            ui->tableWidget->setItem(current_row, 6,
+                                     new QTableWidgetItem(seller.name.c_str()));
+            ui->tableWidget->setItem(
+                current_row, 7,
+                new QTableWidgetItem(QString::number(deal.rate)));
+            ui->tableWidget->setItem(
+                current_row, 8,
+                new QTableWidgetItem(
+                    (std::to_string(deal.last_update.day()) + "." +
+                     std::to_string(deal.last_update.month()) + "." +
+                     std::to_string(deal.last_update.year()))
+                        .c_str()));
+            current_row++;
         }
+    } catch (const std::exception& e) {
+        QMessageBox::critical(this, "Ошибка", e.what());
+        return;
     }
 }
 
@@ -134,33 +156,38 @@ void leaderWidget::on_Stock_clicked() {
 
     ui->ProviderLabel->setText("Склад");
     QStringList Labels = {"ID",         "Ноутбук",  "Цена",
-
-    // TODO: fill table with db
-    // tableStorageUpdate();
-
-    // get from db
-    ui->tableWidget->setRowCount(5);
-    ui->tableWidget->setColumnCount(6);
-    QStringList Labels = {"id",     "id laptop", "price",
-                          "number", "available", "source"};
+                          "Количество", "Доступно", "Поставщик"};
+    ui->tableWidget->setColumnCount(Labels.size());
     ui->tableWidget->setHorizontalHeaderLabels(Labels);
 
-    ui->tableWidget->setColumnWidth(0, 78);
-    ui->tableWidget->setColumnWidth(1, 78);
-    ui->tableWidget->setColumnWidth(2, 78);
-    ui->tableWidget->setColumnWidth(3, 77);
-    ui->tableWidget->setColumnWidth(4, 77);
-    ui->tableWidget->setColumnWidth(5, 78);
-
-    // temporary
-    QTableWidgetItem* item = new QTableWidgetItem("123");
-    for (size_t i = 0; i < 5; i++) {
-        for (size_t j = 0; j < 6; j++) {
-            QTableWidgetItem* item = new QTableWidgetItem("123");
-            ui->tableWidget->setItem(i, j, item);
-            ui->tableWidget->item(i, j)->setFlags(Qt::ItemIsEnabled |
-                                                  Qt::ItemIsSelectable);
+    try {
+        soci::session session(*parent->database.get_pool().lock());
+        std::vector<boost::tuple<stock, laptop, provider>> result =
+            getStock(session, 0, 10);
+        ui->tableWidget->setRowCount(result.size());
+        size_t current_row = 0;
+        for (const auto& [stock, laptop, provider] : result) {
+            ui->tableWidget->setItem(
+                current_row, 0,
+                new QTableWidgetItem(QString::number(stock.id)));
+            ui->tableWidget->setItem(current_row, 1,
+                                     new QTableWidgetItem(laptop.name.c_str()));
+            ui->tableWidget->setItem(
+                current_row, 2,
+                new QTableWidgetItem(QString::number(stock.price)));
+            ui->tableWidget->setItem(
+                current_row, 3,
+                new QTableWidgetItem(QString::number(stock.count)));
+            ui->tableWidget->setItem(
+                current_row, 4,
+                new QTableWidgetItem(QString::number(stock.available)));
+            ui->tableWidget->setItem(
+                current_row, 5, new QTableWidgetItem(provider.name.c_str()));
+            current_row++;
         }
+    } catch (const std::exception& e) {
+        QMessageBox::critical(this, "Ошибка", e.what());
+        return;
     }
 }
 
