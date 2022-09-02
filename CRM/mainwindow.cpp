f03cdc9ec74e3d6d4f59cd0a1b72b3c3e6985c81
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2022-09-02 22:59:58 +0300

    Улучшен интерфейс продавца

--- CRM/mainwindow.cpp
+++ CRM/mainwindow.cpp
@@ -34,7 +34,7 @@ MainWindow::~MainWindow() { delete ui; }
 
 void MainWindow::setSellerInterface(std::string name, std::string surname) {
     setCentralWidget(new sellerWidget(this, name, surname));
-    setFixedSize({821, 511});
+    setFixedSize({822, 494});
 }
 
 void MainWindow::setMarketerInterface(std::string name, std::string surname) {
